ad2f28037a57c1656772b1bc55cc39fc0a1aac27
Pei-Ming Wu <p408865@gmail.com>
2018-11-08 22:45:34 +0800

    add keyboard events

--- canvas.js
+++ canvas.js
@@ -1,8 +1,10 @@
+const width = 800, height = 500;
+
 let canvas = document.querySelector('#draw');
 // could be 3d, if you want to make a video game
 let ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 400;
+canvas.width = width;
+canvas.height = height;
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -16,6 +18,7 @@ function draw(e) {
     // stop the function if they are not mouse down
     // listen for mouse move event
     if (lastX !== undefined && lastY !== undefined) {
+        ctx.beginPath();
         ctx.moveTo(lastX, lastY);
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
@@ -25,22 +28,22 @@ function draw(e) {
 
 canvas.addEventListener('mousemove', draw);
 
-function keyboardfeature(e){
-    if (canvas.keys && canvas.keys[66]) {ctx.strokeStyle = 'blue'; }
-    if (canvas.keys && canvas.keys[71]) {ctx.strokeStyle = 'green'; }
-    if (canvas.keys && canvas.keys[82]) {ctx.strokeStyle = 'red'; }
-    if (canvas.keys && canvas.keys[89]) {ctx.strokeStyle = 'yellow'; }
-    if (canvas.keys && canvas.keys[38]) {ctx.lineWidth +=1; }
-    if (canvas.keys && canvas.keys[40]) {ctx.lineWidth -=1; }
-    if (canvas.keys && canvas.keys[32]) {this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)}
-}
-
-canvas.addEventListener('keyboard',function(e){
-    if (canvas.keys && canvas.keys[66]) {ctx.strokeStyle = 'blue'; }
-    if (canvas.keys && canvas.keys[71]) {ctx.strokeStyle = 'green'; }
-    if (canvas.keys && canvas.keys[82]) {ctx.strokeStyle = 'red'; }
-    if (canvas.keys && canvas.keys[89]) {ctx.strokeStyle = 'yellow'; }
-    if (canvas.keys && canvas.keys[38]) {ctx.lineWidth +=1; }
-    if (canvas.keys && canvas.keys[40]) {ctx.lineWidth -=1; }
-    if (canvas.keys && canvas.keys[32]) {this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)}
-})
+document.addEventListener('keydown', (e) => {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+    case 66: // blue
+        ctx.strokeStyle = 'blue'; break;
+    case 82: // red
+        ctx.strokeStyle = 'red'; break;
+    case 89: // yellow
+        ctx.strokeStyle = 'yellow'; break;
+    case 71: // green
+        ctx.strokeStyle = 'green'; break;
+    case 38: // up
+        ctx.lineWidth += 1; break;
+    case 40: // down
+        ctx.lineWidth -= 1; break;
+    case 32: // space
+        ctx.clearRect(0, 0, width, height); break;
+    }
+});
